6f48dcb14cebfffab4bfcc7bf66fd93e7584fb47
blnkuser0 <cenarvaez24@gmail.com>
2026-01-20 15:31:58 +0800

    server.ts set to local

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
-import dotenv from 'dotenv';
+// local server
+import dotenv from "dotenv";
 dotenv.config();
 
-import app from './app';
-import { connectDB } from './config/database';
-import { createAdmin } from './seed/createAdmin';
+import app from "./app";
+import { connectDB } from "./config/database";
+import { createAdmin } from "./seed/createAdmin";
 
 let isConnected = false;
 
@@ -16,9 +17,38 @@ const handler = async (req: any, res: any) => {
     }
     return app(req, res);
   } catch (error) {
-    console.error('Vercel handler error:', error);
-    res.status(500).json({ message: 'Server error' });
+    console.error("Vercel handler error:", error);
+    res.status(500).json({ message: "Server error" });
   }
 };
 
 export default handler;
+
+
+
+
+// vercel server
+// import dotenv from 'dotenv';
+// dotenv.config();
+
+// import app from './app';
+// import { connectDB } from './config/database';
+// import { createAdmin } from './seed/createAdmin';
+
+// let isConnected = false;
+
+// const handler = async (req: any, res: any) => {
+//   try {
+//     if (!isConnected) {
+//       await connectDB();
+//       await createAdmin();
+//       isConnected = true;
+//     }
+//     return app(req, res);
+//   } catch (error) {
+//     console.error('Vercel handler error:', error);
+//     res.status(500).json({ message: 'Server error' });
+//   }
+// };
+
+// export default handler;
